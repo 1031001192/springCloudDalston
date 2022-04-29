4c6b371f065b462dd7e957b1425a4fbf606ff785
1031001192 <1031001192@qq.com>
2022-04-29 18:03:14 +0800

    [futrun] zhujie

---
+++ dalston_consumer/src/main/java/com/consumer/Test/FruitRun.java
@@ -0,0 +1,17 @@
+package com.consumer.Test;
+
+/**
+ * @Description: 输出结果
+ * @Title: FruitRun
+ * @Package com.consumer.Test
+ * @Author: LiBing
+ * @Copyright 版权归个人所有
+ * @CreateTime: 2022/4/29 18:02
+ *
+ * url https://www.cnblogs.com/GarrettWale/p/15610964.html
+ */
+public class FruitRun {
+    public static void main(String[] args) {
+        FruitInfoUtil.getFruitInfo(Apple.class);
+    }
+}
