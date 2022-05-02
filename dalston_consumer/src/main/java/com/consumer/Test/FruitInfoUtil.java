ecd68f40bd57767c4859832e458510e1c7493b11
1031001192 <1031001192@qq.com>
2022-05-02 23:51:30 +0800

    [futrun]  自定义 注解的使用

--- dalston_consumer/src/main/java/com/consumer/Test/FruitInfoUtil.java
+++ dalston_consumer/src/main/java/com/consumer/Test/FruitInfoUtil.java
@@ -15,19 +15,26 @@ public class FruitInfoUtil {
         String strFruitColor=" 水果颜色：";
         String strFruitProvicer="供应商信息：";
 
+        //获得该类 所有字段，包括私有字段，getFields()是获取public字段
         Field[] fields = clazz.getDeclaredFields();
 
+        //循环该类 所有字段
         for(Field field :fields){
+            //判断指定注解 是否在此字段上
+            //水果名称
             if(field.isAnnotationPresent(FruitName.class)){
+                //如果是该类注解，获取注解 取得 改字段的值
                 FruitName fruitName = (FruitName) field.getAnnotation(FruitName.class);
                 strFruitName=strFruitName+fruitName.value();
                 System.out.println(strFruitName);
             }
+            //水果颜色
             else if(field.isAnnotationPresent(FruitColor.class)){
                 FruitColor fruitColor= (FruitColor) field.getAnnotation(FruitColor.class);
                 strFruitColor=strFruitColor+fruitColor.fruitColor().toString();
                 System.out.println(strFruitColor);
             }
+            //水果供应者
             else if(field.isAnnotationPresent(FruitProvider.class)){
                 FruitProvider fruitProvider= (FruitProvider) field.getAnnotation(FruitProvider.class);
                 strFruitProvicer=" 供应商编号："+fruitProvider.id()+" 供应商名称："+fruitProvider.name()+" 供应商地址："+fruitProvider.address();
--- dalston_consumer/src/main/java/com/consumer/Test/FruitName.java
+++ dalston_consumer/src/main/java/com/consumer/Test/FruitName.java
@@ -6,6 +6,32 @@ import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
+ * https://blog.csdn.net/yinni11/article/details/82019967
+ *
+ * https://zhuanlan.zhihu.com/p/213923786
+ *
+ * 下面是元注解：只能用来注解普通注解@Documented @Retention @Target @Inherited
+ *
+ * @Documented 生成到javadoc文档中
+ *
+ * 注解级别  可通过 @Retention 设置 ：SOURCE、CLASS、RUNTIME
+ *
+ * 修饰范围 可通过 @Target 来设置 只修饰 方法 或修饰类
+ *
+ * 注解继承 可通过@Inherited注解的注解 可以被继承(伪继承)
+ * */
+
+/**
+ * 注解i相关方法
+ *
+ * 方法名	用法
+ * Annotation getAnnotation(Class annotationType)	获取注解在其上的annotationType
+ * Annotation[] getAnnotations()	获取所有注解
+ * isAnnotationPresent(Class annotationType)	判断当前元素是否被annotationType注解
+ * Annotation[] getDeclareAnnotations()	与getAnnotations() 类似，但是不包括父类中被Inherited修饰的注解
+ * */
+
+/**
  * 水果名称注解
  */
 @Target(FIELD)
